8badcca953974e6b01d546833b733336cfd3840d
Matt Oestreich <oze41998@yahoo.com>
2019-02-04 15:59:00 -0600

    Add files via upload

---
+++ src/components/selectprovidercard.vue.js
@@ -0,0 +1,25 @@
+Vue.component('v-select-provider-card', {
+    template: `
+    <v-layout align-start justify-center row>
+        <v-flex md8 elevation-5>
+            <v-card class="grey lighten-3">
+                <v-card-title class="justify-center">
+                    <h2>Please select IP API Provider</h2>
+                </v-card-title>
+                <v-sheet d-flex height="70px">
+                    <v-card-title class="layout justify-center">
+                        <h2>Please select IP API Provider</h2>
+                    </v-card-title>
+                </v-sheet>
+            </v-card>
+        </v-flex>
+    </v-layout>    
+    `,
+    /*data() {
+        
+    },*/
+    mounted() {},
+    methods: {
+
+    }
+})
