9bca74ab2c04a8eb83300a0e7f634d395bce98c5
Matt Oestreich <oze41998@yahoo.com>
2019-02-04 10:18:23 -0600

    Add files via upload

---
+++ src/js/app.js
@@ -0,0 +1,28 @@
+new Vue({
+    el: '#app',
+    data: {
+        counter: 0,
+        secondCounter: 0,
+        name: '',
+    },
+    computed: {
+        output: function () {
+            console.log('computed');
+            return this.counter > 5 ? "Greater than 5" : "Smaller than 5";
+        },
+    },
+    watch: {
+        counter: function () {
+            let vm = this;
+            setTimeout(function () {
+                vm.counter = 0;
+            }, 5000);
+        },
+    },
+    methods: {
+        result: function () {
+            console.log('method()');
+            return this.counter > 5 ? "Greater than 5" : "Smaller than 5";
+        },
+    },
+});
