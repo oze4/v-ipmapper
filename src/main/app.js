8f6ef4ef18115a9e89b109bfe8b39a01f6ef62a2
Matt Oestreich <oze41998@yahoo.com>
2019-02-05 16:00:13 -0600

    Update app.js

--- src/main/app.js
+++ src/main/app.js
@@ -16,23 +16,7 @@ new Vue({
             },
         ],
     },
-    computed: {
-        /*result: function () {
-            return this.counter > 5 ? "Greater than 5" : "Smaller than 5";
-        },*/
-    },
-    watch: {
-        /*counter: function () {
-            let vm = this;
-            setTimeout(function () {
-                vm.counter = 0;
-            }, 5000);
-        },*/
-    },
-    methods: {
-        /*result: function () {
-            console.log('method()');
-            return this.counter > 5 ? "Greater than 5" : "Smaller than 5";
-        },*/
-    },
+    computed: {},
+    watch: {},
+    methods: {},
 });
