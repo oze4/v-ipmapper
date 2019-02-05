1567dc234bc89e105df295a09fa9dae92f0cb04f
Matt Oestreich <oze41998@yahoo.com>
2019-02-05 10:39:31 -0600

    Update topbar.vue.js

--- src/components/topbar.vue.js
+++ src/components/topbar.vue.js
@@ -1,6 +1,6 @@
-Vue.component("v-topbar-part", {
+var componentVTopbarPart = {     
     template: `
-    <div style="margin-bottom:90px;">
+    <div class="m-b-90">
       <v-toolbar dark color="primary" fixed app> 
         <v-spacer></v-spacer>
             <a href="https://github.com/oze4" rel="noopener noreferrer" target="_blank" style="margin-top:5px;">
@@ -18,4 +18,4 @@ Vue.component("v-topbar-part", {
     methods: {
 
     }
-})
+}
