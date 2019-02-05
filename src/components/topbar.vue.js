f5d1d9458dd31aebf911b4b7a82250746a80fb28
Matt Oestreich <oze41998@yahoo.com>
2019-02-04 22:10:41 -0600

    Update topbar.vue.js

--- src/components/topbar.vue.js
+++ src/components/topbar.vue.js
@@ -1,22 +1,20 @@
 Vue.component("v-topbar-part", {
     template: `
-    <div>
+    <div style="margin-bottom:60px;">
       <v-toolbar dark color="primary" fixed app> 
         <v-spacer></v-spacer>
-            <!--<v-toolbar-title class="white--text">Title</v-toolbar-title>-->
             <a href="https://github.com/oze4" rel="noopener noreferrer" target="_blank" style="margin-top:5px;">
                 <img style="width: 56px; height: 52px;" alt="homepage" src="./docs/img/ostrike_logo.png">
             </a>
         <v-spacer></v-spacer>
         </v-toolbar>
-        <div style="margin-bottom: 60px;">
-        </div>
     </div>
     `,
-    /*data() {
-        
-    },*/
-    mounted() {},
+    mounted: function () {
+        return {
+
+        };
+    },
     methods: {
 
     }
