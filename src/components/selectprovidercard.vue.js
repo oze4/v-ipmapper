499d5dcb88b4c96043b4190560988d545ee1cd26
Matt Oestreich <oze41998@yahoo.com>
2019-02-05 10:55:44 -0600

    Update selectprovidercard.vue.js

--- src/components/selectprovidercard.vue.js
+++ src/components/selectprovidercard.vue.js
@@ -4,25 +4,25 @@ let componentVSelectProviderCard = {
         <v-flex md8 elevation-5>
             <v-card class="grey lighten-3">
                 <v-card-title class="justify-center">
-                    <v-flex md4 text-md-center>
+                    <div md4 text-md-center>
                         <h2>{{ title }}</h2>    
                         <small>
                             <span class="mdi mdi-key red--text"></span>
                             {{ subTitle }}
                         </small>                                         
-                    </v-flex>
+                    </div>
                 </v-card-title>
             </v-card>
             <v-card>
                 <v-card-title class="justify-center">
-                    <v-flex md4 align-center>
+                    <div md4 align-center>
                         <v-select :searchable=false :options="apiProviders">
                             <template slot="option" slot-scope="option">
                                 {{ option.label }}
                                 <span class="mdi red--text" :class="option.icon"></span>                                
                             </template>
                         </v-select>
-                    </v-flex>
+                    </div>
                 </v-card-title>
             </v-card>
         </v-flex>
