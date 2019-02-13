80f302334265a15fa73b6686137abdf8505b5d66
oze4 <matthewpoestreich@gmail.com>
2019-02-12 23:07:28 -0600

    update to toolbar - testing adding a heading with scroll

--- src/components/topbar.vue.js
+++ src/components/topbar.vue.js
@@ -1,12 +1,14 @@
 let topbar = {     
     template: `
     <div>
-        <v-toolbar dark color="primary" fixed app> 
+        <v-toolbar dark color="primary" fixed flat app> 
             <v-spacer></v-spacer>
             <a href="https://github.com/oze4" rel="noopener noreferrer" target="_blank" style="margin-top:5px;">
                 <img style="width: 56px; height: 52px;" alt="homepage" src="./docs/img/ostrike_logo.png">
             </a>
             <v-spacer></v-spacer>
+        </v-toolbar>
+        <v-toolbar dark color="primary" extended flat style="height:300px;">
         </v-toolbar>
     </div>
     `,
