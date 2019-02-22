9d442152238822a2ab31f82d39001e5540360175
oze4 <matthewpoestreich@gmail.com>
2019-02-21 23:43:49 -0600

    trimming

--- src/components/ipm-generate-map.vue.js
+++ src/components/ipm-generate-map.vue.js
@@ -1,20 +1,16 @@
 let ipm_generate_map = {
     template: `
     <div :style='{ display: shown }'>
-
         <v-layout row wrap justify-center mb-5>
-
             <v-flex xs10 sm10 md10 lg10>
                 <v-card style='overflow-x:auto;' ref='map_container' id='map-container'>
                     <v-card-title class='justify-center'>
-                        <h3>{{ cardTitle }}</h3>
+                        <h3>Click Marker For More Info</h3>
                     </v-card-title>
                     <div id='map-card' ref='map_card' :style="{ height: mapHeight + 'px' }"></div>
                 </v-card>
             </v-flex>
-
         </v-layout>
-
     </div>
     `,
     props: {
@@ -30,83 +26,82 @@ let ipm_generate_map = {
             return JSON.stringify(this.response, undefined, 2);
         },
         mapHeight() {
-            if (this.map.object !== null) {
+            if (this.map !== null) {
                 setTimeout((vm) => {
-                    vm.map.object.invalidateSize();
+                    vm.map.invalidateSize();
                 }, 200, this);
             }
             return this.$vuetify.breakpoint.height - 125;
         },
-        popUpMessage() {
-            return `IP Information:<br/><pre>${this.jsonResponse}</pre>`;
-        }
     },
     data() {
         return {
             isShown: false,
             response: null,
-            cardTitle: 'Click Marker For More Info',
-            map: {
-                object: null,
-                displayElement: 'map-card',
-                titleLayerString: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-                attributionString: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-            },
+            map: null,
         };
     },
     watch: {
         options() {
             if (this.options) {
-                this.mapData(this.options);
+                this.initMap(this.options);
             }
         },
     },
     methods: {
-        initMap(res, ip, lat, lon) {
+        buildMap(res, ip, lat, lon) {
+            this.clearMap();
+            this.isShown = true;
             this.response = res.data;
-            this.map.object = L.map(this.map.displayElement)
+
+            this.map = L.map('map-card')
                 .setView([lat, lon], 13);
 
-            L.tileLayer(this.map.titleLayerString, {
-                attribution: this.map.attributionString,
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
                 maxZoom: 14,
                 minZoom: 10,
-            }).addTo(this.map.object);
+            }).addTo(this.map);
 
             L.marker([lat, lon], {
                 title: `IP: ${ip} | lat: ${lat} | lon: ${lon}`,
                 riseOnHover: true,
-            }).bindPopup(this.popUpMessage, {
+            }).bindPopup(`IP Information:<br/><pre>${this.jsonResponse}</pre>`, {
                 maxWidth: "auto",
                 maxHeight: "auto"
-            }).addTo(this.map.object);
+            }).addTo(this.map);
 
             setTimeout((vm) => {
-                vm.map.object.invalidateSize();
+                vm.map.invalidateSize();
             }, 200, this);
 
         },
         clearMap() {
-            if (this.map.object !== null) {
-                this.map.object.remove();
+            if (this.map !== null) {
+                this.map.remove();
             }
         },
-        mapData(data) {
+        handleAxiosError(url, error) {
+            alert(`Unable to gather map data from ${url}! We encountered the following error: ${error}`);
+        },
+        initMap(data) {
             // this is here to verify what we are being given
             switch (data.provider.name) {
                 case "http://ip-api.com":
                     {
                         // No API key required here, but lets verify
                         if (data.provider.isKeyRequired === false) {
-                            this.clearMap();
                             let h = data.host === '_current_' ? '' : data.host;
                             let u = `http://ip-api.com/json/${String(h)}`;
                             axios.get(u).then((res) => {
-                                this.isShown = true;
-                                let ip = res.data.query;
-                                this.initMap(res, ip, res.data.lat, res.data.lon);
+                                this.buildMap(
+                                    res,
+                                    res.data.query,
+                                    res.data.lat,
+                                    res.data.lon
+                                );
                             }).catch((err) => {
-                                alert(`Unable to gather map data from ${u}! We encountered the following error: ${err}`);
+                                this.handleAxiosError(u, err);
                             });
                         }
                     }
@@ -115,15 +110,17 @@ let ipm_generate_map = {
                     {
                         // API key is required here, lets verify
                         if (data.provider.isKeyRequired === true) {
-                            this.clearMap();
                             let h = data.host === '_current_' ? 'check' : data.host;
                             let u = `http://api.ipstack.com/${String(h)}?access_key=${String(data.apiKey)}`;
                             axios.get(u).then((res) => {
-                                this.isShown = true;
-                                let ip = res.data.ip;
-                                this.initMap(res, ip, res.data.latitude, res.data.longitude);
+                                this.buildMap(
+                                    res,
+                                    res.data.ip,
+                                    res.data.latitude,
+                                    res.data.longitude
+                                );
                             }).catch((err) => {
-                                alert(`Unable to gather map data from ${u}! We encountered the following error: ${err}`);
+                                this.handleAxiosError(u, err);
                             });
                         }
                     }
